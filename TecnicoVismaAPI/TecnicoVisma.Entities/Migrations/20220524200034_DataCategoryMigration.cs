3a7d521ba652041b7ba127bfcb28b56be49a7d9b
Adam <Adam@DESKTOP-GM6IUPR>
2022-05-24 17:10:03 -0300

    Docker configuration

---
+++ TecnicoVismaAPI/TecnicoVisma.Entities/Migrations/20220524200034_DataCategoryMigration.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TecnicoVisma.Entities.Migrations
+{
+    public partial class DataCategoryMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Clothes" },
+                    { 2, "Home" },
+                    { 3, "Electronic" },
+                    { 4, "Software" },
+                    { 5, "Video Games" },
+                    { 6, "Vehicles" },
+                    { 7, "Footwear" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 7);
+        }
+    }
+}
--- TecnicoVismaAPI/TecnicoVisma.Repositories/UserRepository.cs
+++ TecnicoVismaAPI/TecnicoVisma.Repositories/UserRepository.cs
@@ -7,6 +7,7 @@ using TecnicoVisma.Entities.Data;
 using TecnicoVisma.Entities.DTOS;
 using TecnicoVisma.Entities.Models;
 using TecnicoVisma.Interfaces;
+using System.Security.Cryptography;
 
 namespace TecnicoVisma.Repositories
 {
@@ -20,8 +21,25 @@ namespace TecnicoVisma.Repositories
 
         public User AuthenticateUser(AuthenticateDTO authenticateDTO)
         {
-            var user = _context.Users.FirstOrDefault(x => x.MailAddress == authenticateDTO.MailAddress && x.Password == authenticateDTO.Password);
+
+            var user = _context.Users.FirstOrDefault(x => x.MailAddress == authenticateDTO.MailAddress && x.Password == sha256_hash(authenticateDTO.Password));
             return user;
+        }
+
+        public static String sha256_hash(string value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
         }
 
         public User CheckUserAvailabity(string mailAddress)
@@ -38,6 +56,7 @@ namespace TecnicoVisma.Repositories
 
         public User Insert(User user)
         {
+            user.Password = sha256_hash(user.Password);
             _context.Users.Add(user);
             _context.SaveChanges();
             return user;
